0b3aefdba5d528c96c8ee004a8becfb79481c013
Karen Sonnenberg <kjsonnenberg@me.com>
2022-12-18 11:20:16 -0500

    made migration changed date field

--- penner/models.py
+++ penner/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+from datetime import date
 from django.contrib.auth.models import User
 
 LOCATION_CHOICES = [
@@ -16,13 +16,13 @@ LOCATION_CHOICES = [
 class Expense(models.Model):
     name = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    buyer = models.ForeignKey(User,
+    buyer = models.ForeignKey(User, default='2',
                                on_delete=models.CASCADE,
                                related_name='penner_expense')
     location = models.CharField(max_length=2, choices=LOCATION_CHOICES, default="Thrift")
     amount = models.DecimalField(decimal_places=2,max_digits=10)
-    notes = models.TextField()
-    purchased = models.DateTimeField(default=timezone.now)
+    notes = models.TextField(blank=True, null=True)
+    purchased = models.DateField(default=date.today)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
