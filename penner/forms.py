c885368d342cce00866a3fe55fc123c136f53d1f
Karen Sonnenberg <kjsonnenberg@me.com>
2023-02-09 16:52:37 -0500

    trying widget again

--- penner/forms.py
+++ penner/forms.py
@@ -3,6 +3,9 @@ from .models import Expense
 
 
 class ExpenseForm(forms.ModelForm):
+    purchased = forms.DateField(widget=forms.TextInput(attrs= {
+      'class':'datepicker'
+    }))
     class Meta:
         model=Expense
         fields = ['location', 'name', 'amount','purchased']
