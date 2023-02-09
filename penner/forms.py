0385e6c4621aa07739f6ccce59ad4c9d74c15ee9
Karen Sonnenberg <kjsonnenberg@me.com>
2023-02-09 16:15:54 -0500

    styling added

--- penner/forms.py
+++ penner/forms.py
@@ -2,6 +2,28 @@ from django import forms
 from .models import Expense
 
 class ExpenseForm(forms.ModelForm):
+    
+    name = forms.CharField(
+        widget=forms.TextInput(
+                            attrs={
+                                'style' : 'width: 150px;',
+                                'class' : 'input is-small mb-2 mt-1',
+                                
+                            }))
+    location = forms.CharField(
+        widget=forms.ChoiceField(
+                            attrs={
+                                'style' : 'width: 150px;',
+                                'class' : 'input is-small mb-2 mt-1',
+                                
+                            }))
+    amount = forms.CharField(
+        widget=forms.TextInput(
+                            attrs={
+                                'style' : 'width: 50px;',
+                                'class' : 'input is-small mb-2 mt-1',
+                                
+                            }))
     class Meta:
         model=Expense
-        fields = ['name', 'location', 'amount', 'notes']
+        fields = ['name', 'location', 'amount']
