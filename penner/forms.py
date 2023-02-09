5fa4d03ef072465bd5cf1f72e8dd2eac5f955edc
Karen Sonnenberg <kjsonnenberg@me.com>
2023-02-09 16:59:30 -0500

    save

--- penner/forms.py
+++ penner/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from .models import Expense
 
-
+class DateInput(forms.DateInput):
+    input_type = 'date'
+    
 class ExpenseForm(forms.ModelForm):
-    purchased = forms.DateField(widget=forms.TextInput(attrs= {
-      'class':'datepicker'
-    }))
+    purchased = forms.DateField(widget=DateInput)
     class Meta:
         model=Expense
         fields = ['location', 'name', 'amount','purchased']
