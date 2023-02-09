9918301eed5e62f07dde1e8159d9a02ccc8d77b2
Karen Sonnenberg <kjsonnenberg@me.com>
2023-02-09 16:44:58 -0500

    uninstalled datepicker

--- penner/forms.py
+++ penner/forms.py
@@ -1,12 +1,10 @@
 from django import forms
 from .models import Expense
-from bootstrap_datepicker_plus.widgets import DatePickerInput
+
 
 class ExpenseForm(forms.ModelForm):
     class Meta:
         model=Expense
         fields = ['name', 'location', 'amount','purchased']
-        widgets = {
-            'purchased': DatePickerInput
-        }
+        
         
