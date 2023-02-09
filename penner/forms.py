11304beabb48be06eb925f6ed8785f3d197f4065
Karen Sonnenberg <kjsonnenberg@me.com>
2023-02-09 16:22:33 -0500

    without styling fin forms.py

--- penner/forms.py
+++ penner/forms.py
@@ -3,27 +3,27 @@ from .models import Expense
 
 class ExpenseForm(forms.ModelForm):
     
-    name = forms.CharField(
-        widget=forms.TextInput(
-                            attrs={
-                                'style' : 'width: 150px;',
-                                'class' : 'input is-small mb-2 mt-1',
+    # name = forms.CharField(
+    #     widget=forms.TextInput(
+    #                         attrs={
+    #                             'style' : 'width: 150px;',
+    #                             'class' : 'input is-small mb-2 mt-1',
                                 
-                            }))
-    location = forms.CharField(
-        widget=forms.ChoiceField(
-                            attrs={
-                                'style' : 'width: 150px;',
-                                'class' : 'input is-small mb-2 mt-1',
+    #                         }))
+    # location = forms.CharField(
+    #     widget=forms.ChoiceField(
+    #                         attrs={
+    #                             'style' : 'width: 150px;',
+    #                             'class' : 'input is-small mb-2 mt-1',
                                 
-                            }))
-    amount = forms.DecimalField(
-        widget=forms.TextInput(
-                            attrs={
-                                'style' : 'width: 50px;',
-                                'class' : 'input is-small mb-2 mt-1',
+    #                         }))
+    # amount = forms.DecimalField(
+    #     widget=forms.TextInput(
+    #                         attrs={
+    #                             'style' : 'width: 50px;',
+    #                             'class' : 'input is-small mb-2 mt-1',
                                 
-                            }))
+    #                         }))
     class Meta:
         model=Expense
         fields = ['name', 'location', 'amount','purchased']
