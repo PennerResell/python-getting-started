de9a2eafc4d572c69985b413cc5eefa208bfe7c0
Karen Sonnenberg <kjsonnenberg@me.com>
2023-02-09 16:28:49 -0500

    dickpicker added

--- penner/forms.py
+++ penner/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Expense
+from bootstrap_datepicker_plus.widgets import DatePickerInput
 
 class ExpenseForm(forms.ModelForm):
     
@@ -27,3 +28,6 @@ class ExpenseForm(forms.ModelForm):
     class Meta:
         model=Expense
         fields = ['name', 'location', 'amount','purchased']
+        widgets = {
+            'purchased':DatePickerInput()
+        }
